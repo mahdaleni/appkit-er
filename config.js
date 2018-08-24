33de1b863de9f057c9e3b2d4435feebda7f27830
Arif <dewisandrax@gmail.com>
2018-08-24 10:33:18 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'demo',
+		wp_ws_url : 'http://zulu.rf.gd/wp-appkit-api/demo',
+		wp_url : 'http://zulu.rf.gd',
+		theme : 'q-android',
+		version : '1.2.5',
+		app_type : 'phonegap-build',
+		app_title : 'Hello',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'B3*P-uj7[9 xA0Q6$jhCxDMVX?lW>)?76gDw#B:BS_A,5(vo!sFz?zxIyXAqHsg ',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
